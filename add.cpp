62b690ab5f6e8ca075ff4dd10d32d0c94c8bf211
Pham1234568 <tungpham02363758@gmail.com>
2024-12-10 13:45:33 +0700

    add file

---
+++ add.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "support.h"
+
+int add(int a, int b){
+    return a+b;
+}
+int subtract(int a, int b){
+
+    return a-b;
+}
---
+++ header.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "support.h"
+
+using namespace std;
+int main(){
+    cout<<add(3,5)<<endl;
+    cout<<"Hieu 2 so:"<<subtract(9,5)<<endl;
+    return 0;
+}
